5731a66d889f0d05b49142a2e82d4b0f95e5029e
klyuchukviktoriya <klyuchuk.viktoriya@gmail.com>
2026-05-18 12:00:00 +0300

    practice

---
+++ tasks-goit/3-module.js
@@ -0,0 +1,26 @@
+// ! 1
+// // Оголоси змінну fruits. Надай змінній fruits наступне значення: масив фруктів - рядків "apple", "plum", "pear" і "orange".
+
+// let fruits;
+// fruits = ["apple", "plum", "pear", "orange"];
+
+// ! 2
+// // Оголоси три змінні і надай кожній змінній відповідне значення, використовуючи нотацію квадратних дужок.
+// // Ім'я змінної   -  	Значення змінної
+// // firstElement   -  	перший елемент масиву
+// // secondElement  -  	другий елемент масиву
+// // lastElement	  -   останній елемент масиву
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits[fruits.length - 1];
+
+// ! 3
+// // Виконай перевизначення значення елементів з індексами 1 і 3. Заміни "plum" на "peach", а "orange" на "banana".
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// fruits[1] = "peach";
+// fruits[3] = 'banana';
