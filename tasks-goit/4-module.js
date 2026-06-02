1969d9101a4d0b5ae19767b8829e1724a62c0277
klyuchukviktoriya <klyuchuk.viktoriya@gmail.com>
2026-06-02 22:14:06 +0300

    goit tasks

--- tasks-goit/4-module.js
+++ tasks-goit/4-module.js
@@ -313,6 +313,44 @@
 // }
 
 // ! 16
-// Функція getProductPrice(productName) приймає один параметр productName - назва продукту. Функція містить масив об'єктів products з такими властивостями, як name — ім'я товару, price — ціна і quantity — кількість.
+// // Функція getProductPrice(productName) приймає один параметр productName - назва продукту. Функція містить масив об'єктів products з такими властивостями, як name — ім'я товару, price — ціна і quantity — кількість.
 
-// Доповни код функції так, щоб вона шукала об'єкт продукту з певним ім'ям (властивість name) в масиві products і повертала його ціну (властивість price). Якщо продукт з такою назвою не знайдений, функція повинна повертати null.
+// // Доповни код функції так, щоб вона шукала об'єкт продукту з певним ім'ям (властивість name) в масиві products і повертала його ціну (властивість price). Якщо продукт з такою назвою не знайдений, функція повинна повертати null.
+
+// function getProductPrice(productName) {
+//     const products = [
+//         { name: 'Radar', price: 1300, quantity: 4 },
+//         { name: 'Scanner', price: 2700, quantity: 3 },
+//         { name: 'Droid', price: 400, quantity: 7 },
+//         { name: 'Grip', price: 1200, quantity: 9 },
+//     ];
+//     for (const item of products) {
+//         if (item.name === productName) {
+//             return item.price;
+//         }
+//     }
+//     return null;
+// }
+
+// ! 17
+// Напиши функцію getAllPropValues(propName), яка приймає один параметр propName - ім'я (ключ) властивості. Функція повинна повернути масив усіх значень властивості з таким ім'ям з кожного об'єкта в масиві products. Якщо в об'єктах відсутні властивості з таким ім'ям, функція повинна повернути порожній масив.
+
+function getAllPropValues(propName) {
+    const products = [
+        { name: 'Radar', price: 1300, quantity: 4 },
+        { name: 'Scanner', price: 2700, quantity: 3 },
+        { name: 'Droid', price: 400, quantity: 7 },
+        { name: 'Grip', price: 1200, quantity: 9 },
+    ];
+
+    const values = [];
+
+    for (item of products) {
+        if (item.hasOwnProperty(propName)) {
+            values.push(item[propName]);
+        }
+    }
+    return values;
+}
+console.log(getAllPropValues('price'));
+console.log(getAllPropValues('pvvv'));
