0566fd6bb9823279ca05b6051f980fa3069ade7c
klyuchukviktoriya <klyuchuk.viktoriya@gmail.com>
2026-07-01 20:00:00 +0300

    udemy tasks

--- mini-projects.js/fetch-advanced.js
+++ mini-projects.js/fetch-advanced.js
@@ -61,3 +61,108 @@ async function getUsersWithPostsCount() {
     }
 }
 getUsersWithPostsCount().then((info) => console.log(info));
+
+//! Напиши универсальную функцию:
+//
+// async function fetchJSON(url)
+//
+// ======================
+// Что она должна делать
+// ======================
+//
+// 1. Выполнить fetch(url).
+//
+// 2. Проверить response.ok.
+//
+// 3. Если response.ok === false,
+//    выбросить ошибку:
+//
+//    throw new Error(`HTTP Error: ${response.status}`);
+//
+// 4. Если всё хорошо,
+//    вернуть результат response.json().
+//
+
+async function fetchJSON(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP Error: ${response.status}`);
+        }
+        const users = await response.json();
+        return users;
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+fetchJSON('https://jsonplaceholder.typicode.com/users')
+    .then((users) => console.log(users.length))
+    .catch((error) => console.log(error.message));
+
+// Должно вывести:
+//
+// 10
+
+// ======================
+// Проверка №2
+// ======================
+
+fetchJSON('https://jsonplaceholder.typicode.com/userss')
+    .then((users) => console.log(users))
+    .catch((error) => console.log(error.message));
+
+// Должно вывести:
+//
+// HTTP Error: 404
+
+//! fetch-post
+
+// Отправь нового пользователя
+// на сервер.
+//
+// URL:
+//
+// https://jsonplaceholder.typicode.com/users
+//
+// ======================
+// Что нужно сделать
+// ======================
+//
+// Используй fetch с:
+//
+// method: "POST"
+//
+// headers:
+// {
+//   "Content-Type": "application/json"
+// }
+//
+// body:
+// {
+//   name: "Alex",
+//   age: 25
+// }
+//
+// (body нужно преобразовать в JSON)
+
+async function postUser() {
+    try {
+        const response = await fetch(URL_USERS, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                name: 'Alex',
+                age: 25,
+            }),
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP Error: ${response.status}`);
+        }
+        return response.json();
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+postUser().then((response) => console.log(response));
+// Выведи в консоль ответ сервера.
