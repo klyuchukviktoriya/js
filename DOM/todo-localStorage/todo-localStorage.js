ea845dc0bca621d0a2d1ee7e838207e3afd0e5e1
klyuchukviktoriya <klyuchuk.viktoriya@gmail.com>
2026-07-10 20:00:00 +0300

    localStorage todo

--- DOM/todo-localStorage/todo-localStorage.js
+++ DOM/todo-localStorage/todo-localStorage.js
@@ -1,7 +1,11 @@
-
 const input = document.querySelector('.input');
 const addBtn = document.querySelector('.add-button');
 const list = document.querySelector('.list');
+const deleteCompleted = document.querySelector('.delete-completed');
+const totalSpan = document.querySelector('.total-count');
+const activeSpan = document.querySelector('.active-count');
+const completedSpan = document.querySelector('.completed-count');
+
 let todos = JSON.parse(localStorage.getItem('todos')) || [];
 
 renderList();
@@ -27,10 +31,13 @@ addBtn.addEventListener('click', () => {
 function renderList() {
   list.innerHTML = todos
     .map((obj) => `<li data-id='${obj.id}'>
-    <p>${obj.text}</p>
+    <p class="${obj.completed ? 'completed' : ''}">${obj.text}</p>
     <button>Delete</button>
     </li>`)
     .join('');
+  totalSpan.textContent = todos.length;
+  activeSpan.textContent = todos.filter(obj => !obj.completed).length;
+  completedSpan.textContent = todos.filter(obj => obj.completed).length;
 }
 
 function saveTodos() {
@@ -38,14 +45,35 @@ function saveTodos() {
 }
 
 list.addEventListener('click', (e) => {
+
   if (e.target.tagName === 'BUTTON') {
     const li = e.target.closest('li');
     const taskId = li.dataset.id;
     const index = todos.findIndex(obj => obj.id === Number(taskId));
+
+
     if (index !== -1) {
       todos.splice(index, 1);
       saveTodos();
       renderList();
     }
   }
+
+  if (e.target.tagName === 'P') {
+    const li = e.target.closest('li');
+    const taskId = li.dataset.id;
+    const task = todos.find(obj => obj.id === Number(taskId));
+
+    if (task) {
+      task.completed = !task.completed;
+      saveTodos();
+      renderList();
+    }
+  }
+})
+
+deleteCompleted.addEventListener('click', () => {
+  todos = todos.filter(obj => obj.completed === false);
+  saveTodos();
+  renderList();
 })
