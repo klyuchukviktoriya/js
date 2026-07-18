24cd9fed3a9e740f9dcf8ae056cfb82d682306ac
klyuchukviktoriya <klyuchuk.viktoriya@gmail.com>
2026-07-19 00:11:21 +0300

    goit hw-7 / DOM

---
+++ hw-goit/7-hw/js/task-4.js
@@ -0,0 +1,31 @@
+//! Завдання 4
+
+// Напиши скрипт управління формою логіна.
+
+// відправлення форми form.login-form повинна відбуватися за подією submit.
+// Під час відправлення форми сторінка не повинна перезавантажуватися.
+// Якщо при сабміті у формі є незаповнені поля, виводь alert з попередженням про те, що 'All form fields must be filled in'. Не додавай на інпути атрибут required, валідація має відбуватися саме через JS.
+// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт з двома властивостями, де ключ — це ім'я інпутів, а значення — відповідні значення цих інпутів, очищені від пробілів по краях. Для доступу до елементів форми використовуй властивість elements.
+// При сабміті форми виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = e.target.elements.email.value.trim();
+  const password = e.target.elements.password.value.trim();
+
+  if (!email || !password) {
+    alert('All form fields must be filled in');
+  } else {
+
+    console.log({
+      email: email,
+      password: password
+    });
+
+    form.reset();
+  }
+
+})
+
